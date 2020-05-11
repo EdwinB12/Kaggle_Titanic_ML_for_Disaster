3d7bf1c6150e36600fe80819ece26c9f68b85598
Edwin <w.edwin.brown@outlook.com>
2020-05-11 10:41:50 +0100

    Add Data Directory, Files and Read_Data.py

---
+++ Read_Data.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 10:34:21 2020
+
+Reading in Kaggle Titanic: Machine Learning for a Disaster
+
+@author: edwin
+"""
+
+print('\n')
+print('--------------- Start --------------------')
+import time
+start=time.time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(time.time()-start)
+print('\n' '\n')
+print('--------------- End --------------------')
