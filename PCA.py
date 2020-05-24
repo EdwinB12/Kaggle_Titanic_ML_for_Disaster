4bd07d023c92d3e10ccca9351a816707d9dfb8b6
Edwin <w.edwin.brown@outlook.com>
2020-05-24 10:39:11 +0100

    PCA Clean up

--- PCA.py
+++ PCA.py
@@ -34,37 +34,48 @@ print('\n')
 print('--------------- Start --------------------')
 print('\n' '\n')
 start=time.time()
+#%%
 
+# ------------------------------ Prep 1 ---------------------------------------
 
-
-# -------------------- Preparing data -------------------
-
-# Prep data for Pipes - Training Data
+# Read Train and Test Datasets and save off original copies
 path = 'Original_Data/train.csv'
 train_original = mlib.csv_to_df(path)
+train_df = train_original.copy()
+path = 'Original_Data/test.csv'
+test_original = mlib.csv_to_df(path)
+test_df = test_original.copy()
+
+# # Create list of features desired for training
 feature_list = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare', 'Embarked']
 target_list = ['Survived']
 
-# Define Numeric Pipeline - Standard Scaling
+# Define Numeric Pipeline
 num_pipe = Pipeline([
     ('imputer_mean', SimpleImputer(strategy='mean')),
     ('std_scalar', StandardScaler())
 ])
 
-# Define Categorical Pipeline - One Hot Enconding
+# Define Categorical Pipeline
 cat_pipe = Pipeline([
     ('imputer' , SimpleImputer(strategy = 'most_frequent')),
-    #('ohe' , OneHotEncoder()),
+    # ('ohe' , OneHotEncoder()),
     ('oe', OrdinalEncoder())
-    
 ])
 
-#Combinin Pipes into full pipeline 
-full_pipeline,train_features,target_features, post_transform_train_features = mlib.Full_PipeLine(
-    path,feature_list,target_list,num_pipe, cat_pipe)
+#Combining Pipes into full pipeline - Train Data
+full_pipeline,train_features,target_features,post_trans_train_feature = mlib.Full_PipeLine(
+    train_df,feature_list,target_list,num_pipe,cat_pipe)
 
-# Transform data using final combined pipeline
+# Combining Pipes into full pipeline - Test Data
+full_pipeline_test,test_features,empty, post_transform_test_features = mlib.Full_PipeLine(
+    test_df,feature_list,[],num_pipe,cat_pipe)
+
+# Transform data using final combined pipeline - Train
 train_features_prep = full_pipeline.fit_transform(train_features)
+
+# Transform data using final combined pipeline - Test
+test_features_prep = full_pipeline.fit_transform(test_features)
 
 
 # ----------------------------- PCA Analysis ---------------------------------
@@ -72,7 +83,7 @@ train_features_prep = full_pipeline.fit_transform(train_features)
 # Printing variance ratio's
 pca = PCA()
 pca.fit(train_features_prep)
-print(pca.explained_variance_ratio_) # Printing variance ratios
+print('Prep1:' , pca.explained_variance_ratio_) # Printing variance ratios
 
 # Plotting Scree Plot
 mlib.Scree_Plot(train_features_prep)
@@ -80,12 +91,81 @@ mlib.Scree_Plot(train_features_prep)
 # Scatter Matrix of PCs
 mlib.PC_CrossPlotting_Color(train_features_prep, target_features)
 
+
+
+#%% ----------------------------- Prep 2 ----------------------------------------
+
+# Read Train and Test Datasets and save off original copies
+train_path = 'Original_Data/train.csv'
+train_original = pd.read_csv(train_path)
+train_df = train_original.copy()
+test_path = 'Original_Data/test.csv'
+test_original = pd.read_csv(test_path)
+test_df = test_original.copy()
+
+# Feature Engineering 
+train_df = mlib.Feature_Engineering(train_df)
+test_df = mlib.Feature_Engineering(test_df)
+
+# Create list of features desired for training
+feature_list = ['Pclass', 'Sex', 'AgeInt', 'IsAlone', 'FareInt', 'Embarked','Title']
+target_list = ['Survived']
+
+# Define Numeric Pipeline
+num_pipe = Pipeline([
+    ('imputer_mean', SimpleImputer(strategy='most_frequent')),
+    #('std_scalar', StandardScaler())
+])
+
+# Define Categorical Pipeline 
+cat_pipe = Pipeline([
+    ('imputer' , SimpleImputer(strategy = 'most_frequent')),
+    #('ohe' , OneHotEncoder()),
+    ('oe', OrdinalEncoder())
+    
+])
+
+# Combining Pipes into full pipeline - Training Data
+full_pipeline_train,train_features,target_features, post_transform_train_features = mlib.Full_PipeLine(
+    train_df,feature_list,target_list,num_pipe, cat_pipe)
+
+# Combining Pipes into full pipeline - Test Data
+full_pipeline_test,test_features,empty, post_transform_test_features = mlib.Full_PipeLine(
+    test_df,feature_list,[],num_pipe,cat_pipe)
+
+# Transform data using final combined pipeline
+train_features_prep = full_pipeline_train.fit_transform(train_features)
+test_features_prep = full_pipeline_train.fit_transform(test_features)
+
+
+
+# ---------------------------------------- PCA Prep 2 -------------------------
+
+# Printing variance ratio's
+pca = PCA()
+pca.fit(train_features_prep)
+print('Prep 2:' ,pca.explained_variance_ratio_) # Printing variance ratios
+
+# Plotting Scree Plot
+mlib.Scree_Plot(train_features_prep)
+
+# Cross plot of all principal components
+mlib.PC_CrossPlotting_Color(train_features_prep, target_features)
+
+# # Form prepped data into df if required. 
+# corr_df = pd.DataFrame(data = train_features_prep, columns = post_transform_test_features)
+# corr_df['Survived'] = target_features
+
+
 # ------------------------ Finish --------------------------------------------
     
 print('\n') 
 print('Script runtime:', (time.time()-start)/60)
 print('\n' '\n')
 print('----------------- End --------------------')
+
+# -------------------------------------------------------------------------#
+#---------------------------------------------------------------------------
 
 
 # -------------------------- Bits and Bobs --------------------------------
